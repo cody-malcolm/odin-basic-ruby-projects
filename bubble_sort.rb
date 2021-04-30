c8c64835efca0cbc31bdf22a8e44b4ac0008ac19
cody-malcolm <cody.malcolm@ontariotechu.net>
2021-04-30 13:37:06 -0400

    Enhance solution to handle blocks

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,31 @@
-def bubble_sort(nums)
-  numbers = nums  # do not mutate original array
+def bubble_sort(ary)
+  array = Array.new(ary)  # do not mutate original array
   swapped = true
   while swapped do
     swapped = false
-    numbers.slice(0...-1).each_with_index do |n, i|
-      if numbers[i+1] < numbers[i]
-        temp = numbers[i]
-        numbers[i] = numbers[i+1]
-        numbers[i+1] = temp
-        swapped = true
+    array.slice(0...-1).each_with_index do |n, i|
+      if block_given?
+        if yield(array[i], array[i+1]) > 0
+          array[i], array[i+1] = array[i+1], array[i]
+          swapped = true
+        end
+      else
+        if array[i+1] < array[i]
+          array[i], array[i+1] = array[i+1], array[i]
+          swapped = true
+        end
       end
     end
   end
-  numbers
+  array
 end
 
 numbers_1 = [5, 1, 4, 2, 8]
 numbers_2 = [4, 3, 78, 2, 0, 2]
-p numbers_1
+words_1 = ["hi", "hello", "hey", "hola", "haloo"]
 p bubble_sort(numbers_1)
-p numbers_2
+p numbers_1
 p bubble_sort(numbers_2)
+p numbers_2
+p bubble_sort(words_1) { |l, r| l.length - r.length }
+p words_1
